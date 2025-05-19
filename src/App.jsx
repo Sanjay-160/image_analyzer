a40b591f99c5d69451ed94bd08fcbaf118b4fca6
Sanjay S <sanjaysivakumarma@gmail.com>
2025-05-19 15:44:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const AIImageAnalysis = () => {
   const fileInputRef = useRef(null);
   const pageContainerRef = useRef(null);
 
-  const gemini_api_key = "AIzaSyA69gs1nsB8EUQO8cW1J6vktbXZxUKvX1s"
+  const gemini_api_key = "AIzaSyD50jTxAkimx-7Ixj9fxI3eIGXSb4IPC-0"
 
   // Generative AI setup
   const genAI = new GoogleGenerativeAI(
